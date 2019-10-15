840d24e2fd41a128211ac7a9b3df2cba55ad883d
CSWanigasooriya <chamathwanigasooriya@gmail.com>
2019-10-15 13:45:21 +0530

    First Commit

---
+++ featured.php
@@ -0,0 +1,61 @@
+<div class="section">
+<div class="row">
+    <h4 class="center">Featured Courses</h4>
+   <div class="card-content">
+       <p>Iis ita ipsum senserit, eu quis nescius, probant esse deserunt, consequat 
+           instituendarum si mandaremus, quorum firmissimum deserunt quorum deserunt ita 
+           ita aliqua hic quid ubi anim incurreret id aliquip, amet tractavissent 
+           incididunt quis cernantur. Do dolor culpa ad aliquip. A ingeniis ab probant ea 
+           mandaremus imitarentur sed appellat nam et ipsum sed summis, incididunt ex 
+           iudicem qui duis admodum distinguantur, non quem o quem, ad malis est irure, 
+           ipsum mandaremus despicationes. Nostrud minim </p>
+</div>
+</div>
+<div class="row">
+<div class="col s4">
+<div class="card small z-depth-4">
+<div class="card-image waves-effect waves-block waves-light">
+<img class="activator" src="img/book.jpg">
+</div>
+<div class="card-content">
+<span class="card-title activator grey-text text-darken-4">English Language<i class="material-icons right">more_vert</i></span>
+<p><a href="#">More</a></p>
+</div>
+<div class="card-reveal">
+<span class="card-title grey-text text-darken-4">Card Title<i class="material-icons right">close</i></span>
+<p>Here is some more information about this product that is only revealed once clicked on.</p>
+</div>
+</div>
+</div>
+<div class="col s4">
+<div class="card small z-depth-4">
+<div class="card-image waves-effect waves-block waves-light">
+<img class="activator" src="img/book.jpg">
+</div>
+<div class="card-content">
+<span class="card-title activator grey-text text-darken-4">English Language<i class="material-icons right">more_vert</i></span>
+<p><a href="#">More</a></p>
+</div>
+<div class="card-reveal">
+<span class="card-title grey-text text-darken-4">Card Title<i class="material-icons right">close</i></span>
+<p>Here is some more information about this product that is only revealed once clicked on.</p>
+</div>
+</div>
+</div>
+<div class="col s4">
+<div class="card small z-depth-4">
+<div class="card-image waves-effect waves-block waves-light">
+<img class="activator" src="img/book.jpg">
+</div>
+<div class="card-content">
+<span class="card-title activator grey-text text-darken-4">English Language<i class="material-icons right">more_vert</i></span>
+<p><a href="#">More</a></p>
+</div>
+<div class="card-reveal">
+<span class="card-title grey-text text-darken-4">Card Title<i class="material-icons right">close</i></span>
+<p>Here is some more information about this product that is only revealed once clicked on.</p>
+</div>
+</div>
+</div>
+</div>
+</div>
