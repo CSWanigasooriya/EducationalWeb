85579e4142a98eaa546ec54a6db7d44adb48721c
CSWanigasooriya <chamathwanigasooriya@gmail.com>
2019-10-15 13:44:04 +0530

    First Commit

---
+++ index.php
@@ -0,0 +1,11 @@
+<html>
+<head>
+<?php include 'head.php' ?>
+</head>
+    <body>
+    <?php include 'navbar.php' ?>
+    <?php include 'main.php' ?>
+    <?php include 'footer.php' ?>
+    <?php include 'script.php' ?>
+    </body>
+</html>
