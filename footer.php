01c2c3e0fa60e90276075d86d8260733637b9438
CSWanigasooriya <chamathwanigasooriya@gmail.com>
2019-10-15 13:43:47 +0530

    First Commit

---
+++ footer.php
@@ -0,0 +1,26 @@
+        <footer class="page-footer">
+          <div class="container">
+            <div class="row">
+              <div class="col l6 s12">
+                <h5 class="white-text">CNC INSTITUTE</h5>
+                  <p class="grey-text text-lighten-4">Quo fabulas an laboris ex aliquip tamen nulla doctrina enim. Duis pariatur hic dolor summis, nisi familiaritatem ingeniis quae eiusmod, laboris nulla tamen hic 
+                      nisi sed incurreret noster non consequat illustriora.</p>
+              </div>
+              <div class="col l4 offset-l2 s12">
+                <h5 class="white-text">Help Center</h5>
+                <ul>
+                  <li><a class="grey-text text-lighten-3" href="#!">Email Us</a></li>
+                  <li><a class="grey-text text-lighten-3" href="#!">Link 2</a></li>
+                  <li><a class="grey-text text-lighten-3" href="#!">Link 3</a></li>
+                  <li><a class="grey-text text-lighten-3" href="#!">Link 4</a></li>
+                </ul>
+              </div>
+            </div>
+          </div>
+          <div class="footer-copyright">
+            <div class="container">
+            © 2014 Copyright Text
+            <a class="grey-text text-lighten-4 right" href="#!">More Links</a>
+            </div>
+          </div>
+        </footer>
