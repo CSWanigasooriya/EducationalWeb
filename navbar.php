729737cace88e05b34e511e06454997a66439cbf
CSWanigasooriya <chamathwanigasooriya@gmail.com>
2019-10-15 13:44:24 +0530

    First Commit

---
+++ navbar.php
@@ -0,0 +1,27 @@
+<div class="navbar-fixed">
+       <nav class="nav-extended navbar-offset">
+    <div class="nav-wrapper">
+      <a href="#" class="brand-logo"><img src="img/header.png" class="responsive-image hide-on-med-and-down"><span class="hide-on-large-only">CNC INSTITUTE</span></a>
+      <a href="#" data-target="mobile-demo" class="sidenav-trigger"><i class="material-icons">menu</i></a>
+      <ul id="nav-mobile" class="right hide-on-med-and-down">
+        <li><a href="">English</a></li>
+        <li><a href="">Resourses</a></li>
+        <li><a href="" type="button" class="btn waves-effect btn-large">Get Started</a></li>
+      </ul>
+    </div>
+    <div class="nav-content">
+      <ul class="tabs tabs-transparent">
+        <li class="tab"><a href="index.php" target="_self">Home</a></li>
+        <li class="tab"><a href="" target="_self">About</a></li>
+        <li class="tab"><a href="reserve.php" target="_self">Reservations</a></li>
+        <li class="tab"><a href=""></a></li>
+      </ul>
+    </div>
+  </nav>
+     </div>
+
+  <ul class="sidenav" id="mobile-demo">
+    <li><a href="sass.html">Sass</a></li>
+    <li><a href="badges.html">Components</a></li>
+    <li><a href="collapsible.html">JavaScript</a></li>
+  </ul>
