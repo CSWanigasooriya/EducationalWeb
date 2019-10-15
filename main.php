ecaf332518ef86d57bbc8761b25416af5ea3182c
CSWanigasooriya <chamathwanigasooriya@gmail.com>
2019-10-15 13:45:40 +0530

    First Commit

---
+++ main.php
@@ -0,0 +1,163 @@
+<!--Headline-->
+    <div class="parallax-container">
+    <div class="parallax"><img src="img/book.jpg" width="100%">
+<img src="css/m.png" class="responsive-img" id="main">
+<div class="row">
+<div class="col s12">
+<div class="card-content white-text" id="mcard">
+<span class="card-title"><h1 class="left condensed thin amber-text">Step Into a World of Success</h1></span>
+<p class="white-text">CNC Institute, A leading Corporation in Job-Based Trainning, Creating Thousands of success stories by making students Employable.</p>
+<a class="waves-effect waves-light btn btn-large darken-4" href="">Get Started</a>
+</div>
+<div class="card-content white-text" id="acard">
+    <h4 class="card-title black-text thin">Now you can reserve our halls too!</h4>
+    <a type="button" class="btn btn-large">Reserve</a>
+</div>
+</div>
+</div>
+</div>
+</div>
+
+<!--icon section-->
+<div class="container">
+<div class="section white">
+    <div class="row">
+<div class="col s4">
+<div class="icon-block">
+<h2 class="center"><i class="material-icons gtext text-darken-4" style="font-size: 100px">flash_on</i></h2>
+<h5 class="center">6 Month Courses</h5>
+<p class="light">Quem arbitror arbitrantur. Duis a ab labore offendit qui id ita fugiat legam 
+lorem, mandaremus amet veniam proident enim, commodo domesticarum quo deserunt 
+sed appellat sed illum de ad de culpa esse noster est hic anim pariatur.</p>
+</div>
+</div>  
+<div class="col s4">
+<div class="icon-block">
+<h2 class="center"><i class="material-icons gtext text-darken-4" style="font-size: 100px">settings</i></h2>
+<h5 class="center">NVQ IV Qualification</h5>
+<p class="light">Est excepteur o senserit, appellat aute singulis ea aliquip o incurreret a o 
+enim mandaremus vidisse a ea eu minim minim eram de litteris praetermissum a 
+laboris, senserit est vidisse, est illum nisi id pariatur.</p>
+</div>
+</div>
+<div class="col s4">
+<div class="icon-block">
+<h2 class="center"><i class="material-icons gtext text-darken-4" style="font-size: 100px">group</i></h2>
+<h5 class="center">Learn From Experts</h5>
+
+<p class="light">se voluptate illum sunt qui malis a quibusdam eram aliquip aliquip, quamquam 
+aliqua non proident praesentibus, de elit irure ut doctrina, aute eu an esse 
+eiusmod. Excepteur quem veniam ubi amet.</p>
+</div>
+</div>
+</div>
+</div>
+<div class="row">
+<div class="slider col s6 z-depth-2">
+    <ul class="slides">
+      <li>
+        <img src="img/book.jpg"> <!-- random image -->
+        <div class="caption center-align">
+          <h3>This is our big Tagline!</h3>
+          <h5 class="light grey-text text-lighten-3">Here's our small slogan.</h5>
+        </div>
+      </li>
+      <li>
+        <img src="img/book.jpg"> <!-- random image -->
+        <div class="caption left-align">
+          <h3>Left Aligned Caption</h3>
+          <h5 class="light grey-text text-lighten-3">Here's our small slogan.</h5>
+        </div>
+      </li>
+      <li>
+        <img src="img/book.jpg"> <!-- random image -->
+        <div class="caption right-align">
+          <h3>Right Aligned Caption</h3>
+          <h5 class="light grey-text text-lighten-3">Here's our small slogan.</h5>
+        </div>
+      </li>
+      <li>
+        <img src="img/book.jpg"> <!-- random image -->
+        <div class="caption center-align">
+          <h3>This is our big Tagline!</h3>
+          <h5 class="light grey-text text-lighten-3">Here's our small slogan.</h5>
+        </div>
+      </li>
+    </ul>
+  </div>
+<div class="card offset-s1 col s5 z-depth-0">
+   <div class="card-content">
+    <div class="card-title">What we do here?</div>
+       <p>Iis ita ipsum senserit, eu quis nescius, probant esse deserunt, consequat 
+           instituendarum si mandaremus, quorum firmissimum deserunt quorum deserunt ita 
+           ita aliqua hic quid ubi anim incurreret id aliquip, amet tractavissent 
+           incididunt quis cernantur. Do dolor culpa ad aliquip. A ingeniis ab probant ea 
+           mandaremus imitarentur sed appellat nam et ipsum sed summis, incididunt ex 
+           iudicem qui duis admodum distinguantur, non quem o quem, ad malis est irure, 
+           ipsum mandaremus despicationes. Nostrud minim </p>
+</div> 
+</div>
+</div>
+<div class="section">
+<div class="row">
+    <h4 class="center">Featured Courses</h4>
+   <div class="card-content">
+       <p>Iis ita ipsum senserit, eu quis nescius, probant esse deserunt, consequat 
+           instituendarum si mandaremus, quorum firmissimum deserunt quorum deserunt ita 
+           ita aliqua hic quid ubi anim incurreret id aliquip, amet tractavissent 
+           incididunt quis cernantur. Do dolor culpa ad aliquip. A ingeniis ab probant ea 
+           mandaremus imitarentur sed appellat nam et ipsum sed summis, incididunt ex 
+           iudicem qui duis admodum distinguantur, non quem o quem, ad malis est irure, 
+           ipsum mandaremus despicationes. Nostrud minim </p>
+</div>
+</div>
+<div class="row">
+<div class="col s4">
+<div class="card small z-depth-4">
+<div class="card-image waves-effect waves-block waves-light">
+<img class="activator" src="img/book.jpg">
+</div>
+<div class="card-content">
+<span class="card-title activator grey-text text-darken-4">English Language<i class="material-icons right">more_vert</i></span>
+<p><a href="#">More</a></p>
+</div>
+<div class="card-reveal">
+<span class="card-title grey-text text-darken-4">Card Title<i class="material-icons right">close</i></span>
+<p>Here is some more information about this product that is only revealed once clicked on.</p>
+</div>
+</div>
+</div>
+<div class="col s4">
+<div class="card small z-depth-4">
+<div class="card-image waves-effect waves-block waves-light">
+<img class="activator" src="img/book.jpg">
+</div>
+<div class="card-content">
+<span class="card-title activator grey-text text-darken-4">English Language<i class="material-icons right">more_vert</i></span>
+<p><a href="#">More</a></p>
+</div>
+<div class="card-reveal">
+<span class="card-title grey-text text-darken-4">Card Title<i class="material-icons right">close</i></span>
+<p>Here is some more information about this product that is only revealed once clicked on.</p>
+</div>
+</div>
+</div>
+<div class="col s4">
+<div class="card small z-depth-4">
+<div class="card-image waves-effect waves-block waves-light">
+<img class="activator" src="img/book.jpg">
+</div>
+<div class="card-content">
+<span class="card-title activator grey-text text-darken-4">English Language<i class="material-icons right">more_vert</i></span>
+<p><a href="#">More</a></p>
+</div>
+<div class="card-reveal">
+<span class="card-title grey-text text-darken-4">Card Title<i class="material-icons right">close</i></span>
+<p>Here is some more information about this product that is only revealed once clicked on.</p>
+</div>
+</div>
+</div>
+</div>
+</div>
+</div>
+<br>
