3337b936eb97b7f28924b49de889c8daa40d9cb9
CSWanigasooriya <chamathwanigasooriya@gmail.com>
2019-10-16 22:49:00 +0530

    PDO sql connection

--- connect.php
+++ connect.php
@@ -1,12 +1,3 @@
 <?php
-$dbhost='localhost';
-$dbuser='root';
-$dbpass='';
-$conn = mysql_connect($dbhost,$dbuser,$dbpass);
-if(!$conn)
-{
-    die('Could not connect: ' .mysql_error());
-}
-echo 'Connected successfully'.'<br>';
-//mysql_close($conn);
+$dbname = new PDO('mysql:host=localhost;dbname=cnc;charset=utf8', 'root', '', array(PDO::ATTR_EMULATE_PREPARES => false,PDO::ATTR_ERRMODE => PDO::ERRMODE_EXCEPTION));
 ?>
