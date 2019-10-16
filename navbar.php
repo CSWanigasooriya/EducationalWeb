e991553928dcf8666803d03559a224a7c59c2dcb
CSWanigasooriya <chamathwanigasooriya@gmail.com>
2019-10-16 22:50:31 +0530

    Fixed toggle in tabs

--- navbar.php
+++ navbar.php
@@ -4,17 +4,17 @@
       <a href="#" class="brand-logo"><img src="img/header.png" class="responsive-image hide-on-med-and-down"><span class="hide-on-large-only">CNC INSTITUTE</span></a>
       <a href="#" data-target="mobile-demo" class="sidenav-trigger"><i class="material-icons">menu</i></a>
       <ul id="nav-mobile" class="right hide-on-med-and-down">
-        <li><a href="">English</a></li>
-        <li><a href="">Resourses</a></li>
+        <li><a class="dropdown-trigger" href="" data-target='dropdown2'>English</a></li>
+        <li><a class="dropdown-trigger" href="" data-target='dropdown1'>Resourses<i class="material-icons right">arrow_drop_down</i></a></li>
         <li><a href="" type="button" class="btn waves-effect btn-large">Get Started</a></li>
       </ul>
     </div>
     <div class="nav-content">
       <ul class="tabs tabs-transparent">
-        <li class="tab"><a href="index.php" target="_self">Home</a></li>
-        <li class="tab"><a href="" target="_self">About</a></li>
-        <li class="tab"><a href="reserve.php" target="_self">Reservations</a></li>
-        <li class="tab"><a href=""></a></li>
+        <li class="tab"><a href="index.php">Home</a></li>
+        <li class="tab"><a href="#test2">About</a></li>
+        <li class="tab"><a href="reserve.php">Reservations</a></li>
+        <li class="tab"><a href="#test4"></a></li>
       </ul>
     </div>
   </nav>
@@ -24,4 +24,16 @@
     <li><a href="sass.html">Sass</a></li>
     <li><a href="badges.html">Components</a></li>
     <li><a href="collapsible.html">JavaScript</a></li>
+  </ul>
+
+<ul id='dropdown1' class='dropdown-content'>
+    <li><a href="#!">Pastpapers</a></li>
+    <li class="divider" tabindex="-1"></li>
+    <li><a href="#!">Tutorials</a></li>
+  </ul>
+
+<ul id='dropdown2' class='dropdown-content'>
+    <li><a href="#!">Sinhala</a></li>
+    <li class="divider" tabindex="-1"></li>
+    <li><a href="#!">Tamil</a></li>
   </ul>
